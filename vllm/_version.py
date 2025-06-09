9b1c381f26052de57046d8676e63d3a4594cd64b
Raja <raja.gond@outlook.com>
2025-06-09 05:12:41 +0000

    cleanup

--- vllm/_version.py
+++ vllm/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev7+gbc65c2e.d20250608'
-__version_tuple__ = version_tuple = (0, 1, 'dev7', 'gbc65c2e.d20250608')
+__version__ = version = '0.1.dev8+ge29a8d6.d20250608'
+__version_tuple__ = version_tuple = (0, 1, 'dev8', 'ge29a8d6.d20250608')
